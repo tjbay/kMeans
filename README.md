e3c51c82c64b342c860d0af0c22c603e54043f89
tjbay <tjbay@stanfordalumni.org>
2013-12-20 13:16:24 -0800

    final

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 kMeans
 ======
 
-Implementation of kMeans algorithm that is vectorized and 
+Implementation of kMeans algorithm.
 
 Contains a function that will create a data set.  Will also be able to be used with other data sets.
+
+
+kMeans.py - the kMeans function and otheres function that support it.
+kMeans_test.py - Finds an average performance as a function of number of clusters.
+kMeans_compare.py - runs my kMeans and sklearn kMeans to find average running times.  
--- kMeans_compare.py
+++ kMeans_compare.py
@@ -10,25 +10,27 @@ import sklearn.cluster as sklearn
 dataSet = km.createDataSet(10,50,5,10,1)
 
 # Cluster numbers to test
-Nclusters = 12  # Slightly larger than cluster number
-Num_iters = 2
+Nclusters = 12  # Slightly larger than true cluster number
+Num_iters = 50
 
 # Test my implementation
+
 start_time = time.clock()
 for i in range(Num_iters):
 	final_cluster_pos, cost = km.kMeans(dataSet, Nclusters)
         
 avg_run_time = (time.clock() - start_time)/Num_iters
-print "Average run time per kMeans iteration is", avg_run_time, "s" 
+print "Average run time per my kMeans iteration is", avg_run_time, "s" 
 
 # Test scikit-learn kMeans
+
 start_time_scikit = time.clock()
 
-sklearn.KMeans(init='random', n_clusters=Nclusters, n_init=Num_iters, data=dataSet)
+skm = sklearn.KMeans(init='random', n_clusters=Nclusters, n_init=Num_iters)
+skm.fit(dataSet)
 
 avg_run_time_scikit = (time.clock() - start_time_scikit)/Num_iters
 
+print "Average run time per sklearn kMeans iteration is", avg_run_time_scikit, "s" 
 
-
-
-
+print "On my computer the sklearn implemention runs", avg_run_time/avg_run_time_scikit, "times faster"
